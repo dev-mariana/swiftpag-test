b6401227cd07f5d973e6513c52a9bd565dcf8a8c
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2025-10-19 17:52:00 -0300

    feat: add NotificationLog schema and integrate Mongoose in DatabaseModule

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  NotificationLog,
+  NotificationLogSchema,
+} from './mongo/schemas/notification-log.schema';
 import { PrismaService } from './prisma/prisma.service';
 import { ChargeRepository } from './prisma/repositories/charge.repository';
 
 @Module({
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGODB_URI');
+
+        if (!uri) {
+          throw new Error('MONGO_URI environment variable is not defined');
+        }
+        return { uri };
+      },
+    }),
+    MongooseModule.forFeature([
+      {
+        name: NotificationLog.name,
+        schema: NotificationLogSchema,
+      },
+    ]),
+  ],
   providers: [PrismaService, ChargeRepository],
   exports: [PrismaService, ChargeRepository],
 })
---
+++ src/infra/database/mongo/schemas/notification-log.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: { createdAt: 'received_at' } })
+export class NotificationLog extends Document {
+  @Prop({ required: true })
+  charge_id: string;
+
+  @Prop({ required: true })
+  previous_status: string;
+
+  @Prop({ required: true })
+  new_status: string;
+}
+
+export const NotificationLogSchema =
+  SchemaFactory.createForClass(NotificationLog);
